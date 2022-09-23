c0acded6eb65660bb167530e3926f3ada82d19c4
unknown <BT_4N2_15@BKTECH.LOCAL>
2022-09-23 12:31:23 -0400

    Initial commit

---
+++ src/U2L2Runner.java
@@ -0,0 +1,7 @@
+public class U2L2Runner
+{
+    Rectangle rec1 = new Rectangle(150,200);
+    Rectangle rec2 = new Rectangle();
+    Rectangle rec3 = new Rectangle (100);
+    System.out.println("Hello");
+}
